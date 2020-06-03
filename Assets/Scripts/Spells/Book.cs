585671ef8fbd535563a62b0f12b09210549505c3
request <iamrequest@protonmail.com>
2020-06-02 23:40:01 -0400

    Added animation to book

---
+++ Assets/Scripts/Spells/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Book : MonoBehaviour {
+    private Animator animator;
+
+    private Hand hand;
+    public SteamVR_Action_Boolean openBookAction;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        hand = GetComponentInParent<Hand>();
+
+        openBookAction.AddOnChangeListener(OpenCloseBook, hand.handType);
+    }
+
+    private void OpenCloseBook(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState) {
+        animator.SetBool("isBookOpen", newState);
+    }
+}
