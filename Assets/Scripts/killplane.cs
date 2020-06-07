69304d5e3258aeb6542418540068fde0e255f81d
request <iamrequest@protonmail.com>
2020-06-06 21:15:50 -0400

    Added killplane

---
+++ Assets/Scripts/killplane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class killplane : MonoBehaviour {
+    public Transform spawnTransform;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other == Player.instance.headCollider) {
+            Debug.Log(other);
+            Player.instance.transform.position = spawnTransform.position;
+            Player.instance.transform.rotation = spawnTransform.rotation;
+        }
+    }
+}
