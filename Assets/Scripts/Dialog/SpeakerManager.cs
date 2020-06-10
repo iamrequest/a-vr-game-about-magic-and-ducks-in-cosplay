d2781762f2338336c67fd1c9706700198a7b4f8b
request <iamrequest@protonmail.com>
2020-06-09 22:51:48 -0400

    Added maid character

--- Assets/Scripts/Dialog/SpeakerManager.cs
+++ Assets/Scripts/Dialog/SpeakerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum DialogSpeaker {
-   Me, Wizard, Magician, Momo
+   Me, Wizard, Magician, Momo, Maid
 }
 public class SpeakerManager : MonoBehaviour {
     public static SpeakerManager instance;
