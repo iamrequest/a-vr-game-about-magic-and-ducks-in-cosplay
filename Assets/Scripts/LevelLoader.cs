ab69fa2a696c931e3d1974bcb903d9fa81c4e459
request <iamrequest@protonmail.com>
2020-06-10 22:15:20 -0400

    Added mayor dialog

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+
+// Don't forget to set the Collision Layer to "Player Triggers"
+// Also, don't forget to set this layer so that it only collides with the player layer
+public class LevelLoader : MonoBehaviour {
+    private SteamVR_LoadLevel levelLoader;
+
+    private void Start() {
+        levelLoader = GetComponent<SteamVR_LoadLevel>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        levelLoader.enabled = true;
+    }
+}
