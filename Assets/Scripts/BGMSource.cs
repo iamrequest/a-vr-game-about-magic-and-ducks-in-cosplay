0b9994196debbf833cdc8041e5a4ced27e08c705
request <iamrequest@protonmail.com>
2020-06-11 23:33:39 -0400

    Rebaked lighting, final tweaks

--- Assets/Scripts/BGMSource.cs
+++ Assets/Scripts/BGMSource.cs
@@ -7,12 +7,16 @@ using UnityEngine.Events;
 public class BGMSource : MonoBehaviour {
     public AudioClip audioClip;
     private bool isTriggered;
+    public UnityEvent onBGMStart;
 
     private void OnTriggerEnter(Collider other) {
         if (!isTriggered) {
             if (other.TryGetComponent(out VRAudioSource vrAudioSource)) {
                 vrAudioSource.audioSource.clip = audioClip;
                 vrAudioSource.audioSource.Play();
+
+                // Cheap hack to get the first NPC to chat with the player when the player spawns
+                onBGMStart.Invoke();
                 isTriggered = true;
             }
         }
